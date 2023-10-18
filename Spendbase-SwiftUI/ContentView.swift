1a2c7f432ea48b92242adf0124cff9fb48953eeb
aadzhanow <125435070+aadzhanow@users.noreply.github.com>
2023-10-18 15:53:04 +0300

    Initial Commit

---
+++ Spendbase-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spendbase-SwiftUI
+//
+//  Created by Alisher on 18.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spendbase-SwiftUI/Spendbase_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spendbase_SwiftUIApp.swift
+//  Spendbase-SwiftUI
+//
+//  Created by Alisher on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Spendbase_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
